99f2247f1416e56a7bfa2354afa0fc4a6444796f
Naufal Duta <naufaldutam@gmail.com>
2024-05-25 16:04:37 +0700

    try fetching profile from db

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ app.use(express.json()); //middleware
 
 app.use('/books', bookRoutes)
 app.use('/users', userRoutes)
+app.use('/', (req, res) => {
+    res.send('API is running')
+})
 
 app.listen(process.env.APP_PORT, ()=>{
     console.log(`Server Running at Port ${process.env.APP_PORT}`)
