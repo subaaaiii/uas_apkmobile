7ce92b58cdf9bad673f6fcf7ec1750fc72f72c72
Naufal Duta <naufaldutam@gmail.com>
2024-06-03 04:33:22 +0700

    Revert "add token + migration"
    This reverts commit aea1d4451a47c1cf184683b726fb3332acbf5bb4.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
 
 const bookRoutes = require('./routes/books.js');
 const userRoutes = require('./routes/users.js');
